424a1e435ca336acd216fa846a6cf2c1e7b2f191
Tobias Raatiniemi <me@thedeveloperblog.net>
2013-05-26 11:34:51 +0200

    Added web controller.

---
+++ library/controller/Web.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Net\TheDeveloperBlog\Ramverk\Controller
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+
+	/**
+	 * @package Ramverk
+	 * @subpackage Core
+	 *
+	 * @copyright (c) 2013, Authors
+	 * @copyright (c) 2013, The Developer Blog
+	 * @author Tobias Raatiniemi <me@thedeveloperblog.net>
+	 */
+	class Web
+	{
+		public function delegate()
+		{
+		}
+	}
+}
+// End of file: Web.class.php
+// Location: library/controller/Web.class.php
