11b770bbd63041838e8b145b0d421c3110cc9ef2
Tobias Raatiniemi <me@thedeveloperblog.net>
2013-06-30 14:30:30 +0200

    Added the empty Context class.

---
+++ library/core/Context.class.php
@@ -0,0 +1,29 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | This file is a part of the Ramverk project by TheDeveloperBlog.          |
+// | Copyright (c) 2013, Authors.                                             |
+// | Copyright (c) 2013, TheDeveloperBlog.                                    |
+// +--------------------------------------------------------------------------+
+namespace Net\TheDeveloperBlog\Ramverk\Core
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+	use Net\TheDeveloperBlog\Ramverk;
+
+	/**
+	 *
+	 *
+	 * @package Ramverk
+	 * @subpackage Core
+	 *
+	 * @copyright (c) 2013, Authors
+	 * @copyright (c) 2013, The Developer Blog
+	 * @author Tobias Raatiniemi <me@thedeveloperblog.net>
+	 */
+	class Context
+	{
+	}
+}
+// End of file: Context.class.php
+// Location: library/core/Context.class.php
