0ae5a8147140b1eb59a9166272a6bf5f203d6f6c
Tobias Raatiniemi <me@thedeveloperblog.net>
2013-06-30 14:27:23 +0200

    Modified the initialization script.

--- ramverk.php
+++ ramverk.php
@@ -7,12 +7,12 @@ namespace Net\TheDeveloperBlog\Ramverk
 
 	// Require the absolute core classes needed to bootstrap the framework.
 	// With these included the autoload functionality can handle the rest.
+	require __DIR__ . '/library/loader/Autoload.trait.php';
 	require __DIR__ . '/library/core/Core.class.php';
 
 	// Include the configuration container with its core dependencies.
-	require __DIR__ . '/library/data/Container.class.php';
-	require __DIR__ . '/library/configuration/IUtility.interface.php';
 	require __DIR__ . '/library/configuration/Utility.trait.php';
+	require __DIR__ . '/library/data/Container.class.php';
 	require __DIR__ . '/library/configuration/Container.class.php';
 
 	// Include the configuration handlers required by the core.
