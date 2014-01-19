572d84be1a0a6f266f6fe6053c2480600de300c2
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-01-19 08:27:35 +0100

    Minor modifications for including the framework. Also added version controll.

--- ramverk.php
+++ ramverk.php
@@ -5,32 +5,44 @@ namespace Me\Raatiniemi\Ramverk
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
 
+	// Version controll, since we are using traits and other 5.4.0 features the
+	// minimum required version is 5.4.0.
+	if(version_compare(PHP_VERSION, '5.4.0', '<') >= 0) {
+		echo 'The minimum PHP version requirement for Ramverk is PHP 5.4.0.' . PHP_EOL;
+		echo 'You are running version: ' . PHP_VERSION . PHP_EOL;
+		exit;
+	}
+
+	// Setup the include path for the library.
+	$path = __DIR__ . '/library/';
+	set_include_path($path . PATH_SEPARATOR . get_include_path());
+
 	// Require the absolute core classes needed to bootstrap the framework.
 	// With these included the autoload functionality can handle the rest.
-	require __DIR__ . '/library/loader/Autoload.trait.php';
-	require __DIR__ . '/library/configuration/Utility.trait.php';
-	require __DIR__ . '/library/core/Core.class.php';
-	require __DIR__ . '/library/core/Context.class.php';
+	require 'loader/Autoload.trait.php';
+	require 'configuration/Utility.trait.php';
+	require 'core/Core.class.php';
+	require 'core/Context.class.php';
 
 	// Include the configuration container with its core dependencies.
-	require __DIR__ . '/library/data/Container.class.php';
-	require __DIR__ . '/library/configuration/Container.class.php';
+	require 'data/Container.class.php';
+	require 'configuration/Container.class.php';
 
 	// Include the configuration handlers required by the core.
-	require __DIR__ . '/library/configuration/handler/Cache.class.php';
-	require __DIR__ . '/library/configuration/handler/Parser.class.php';
-	require __DIR__ . '/library/configuration/handler/Factory.class.php';
-	require __DIR__ . '/library/configuration/handler/Handler.class.php';
-	require __DIR__ . '/library/configuration/handler/Autoload.class.php';
+	require 'configuration/handler/Cache.class.php';
+	require 'configuration/handler/Parser.class.php';
+	require 'configuration/handler/Factory.class.php';
+	require 'configuration/handler/Handler.class.php';
+	require 'configuration/handler/Autoload.class.php';
 
 	// Include the extended DOM functionality.
-	require __DIR__ . '/library/data/dom/utility/Value.trait.php';
-	require __DIR__ . '/library/data/dom/Document.class.php';
-	require __DIR__ . '/library/data/dom/Element.class.php';
-	require __DIR__ . '/library/data/dom/Node.class.php';
+	require 'data/dom/utility/Value.trait.php';
+	require 'data/dom/Document.class.php';
+	require 'data/dom/Element.class.php';
+	require 'data/dom/Node.class.php';
 
 	// Include exceptions.
-	require __DIR__ . '/library/exception/Exception.class.php';
+	require 'exception/Exception.class.php';
 }
 // End of file: ramverk.php
 // Location: ramverk.php
