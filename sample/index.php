d98560a28d3895d2a9bbe093cbfb44fca93b32dc
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-01-19 16:03:03 +0100

    More of the sample application.

--- sample/index.php
+++ sample/index.php
@@ -1,5 +1,37 @@
 <?php
 namespace Me\Raatiniemi\Ramverk\Sample
 {
-	require_once '../src/ramverk.php';
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk;
+
+	try {
+		// Require the framework bootstrap file, the autoload functionality
+		// within the framework will handle the inclusion of the other files.
+		$directory = realpath(__DIR__ . '/..');
+		require "{$directory}/src/ramverk.php";
+
+		// Setup the basic application configurations. There're three directives
+		// that need to be defined.
+		$config = new Ramverk\Configuration\Container();
+
+		// 'profile'
+		// Profile for the application. For example, useful when separating configuration
+		// between different profiles, e.g. development and production.
+		$config->set('profile', 'development', TRUE, TRUE);
+
+		// 'directory.core'
+		// Absolute path for the core framework directory.
+		$config->set('directory.core', "{$directory}/src");
+
+		// 'directory.application'
+		// Absolute path for the application directory.
+		$config->set('directory.application', "{$directory}/sample/application");
+	} catch(\Exception $e) {
+		// Render thrown exceptions with the specified template.
+		Ramverk\Exception::render($e, $config);
+	}
 }
+// End of file: index.php
+// Location: sample/index.php
