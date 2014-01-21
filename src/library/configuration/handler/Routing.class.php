45694828e13cd93984ab17236655ae5c8ecd5e23
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-01-21 05:16:31 +0100

    Added base for routing configuration handler.

---
+++ src/library/configuration/handler/Routing.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Configuration\Handler
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk;
+	use Me\Raatiniemi\Ramverk\Configuration;
+	use Me\Raatiniemi\Ramverk\Data\Dom;
+
+	/**
+	 * Handler for routing configuration.
+	 *
+	 * @package Ramverk
+	 * @subpackage Configuration
+	 *
+	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+	 * @copyright (c) 2013-2014, Authors
+	 */
+	class Routing extends Configuration\Handler
+	{
+	}
+}
+// End of file: Routing.class.php
+// Location: library/configuration/handler/Routing.class.php
---
+++ test/library/configuration/handler/Routing.test.php
@@ -0,0 +1,27 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Test\Configuration\Handler
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk\Configuration;
+	use Me\Raatiniemi\Ramverk\Configuration\Handler;
+	use Me\Raatiniemi\Ramverk\Data\Dom;
+
+	\Mock::generate('\\Me\\Raatiniemi\\Ramverk\\Configuration\\Container', 'MockContainer');
+
+	/**
+	 * Unit test case for the routing configuration handler.
+	 *
+	 * @package Ramverk
+	 * @subpackage Test
+	 *
+	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+	 * @copyright (c) 2013-2014, Authors
+	 */
+	class Routing extends \UnitTestCase
+	{
+	}
+}
+// End of file: Routing.test.php
+// Location: test/library/configuration/handler/Routing.test.php
