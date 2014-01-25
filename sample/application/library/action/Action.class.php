e0cfdbd90c48f4efc6bb8cdb009a25e1194542bf
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-01-25 07:34:44 +0100

    Added library base action/view classes for the sample application.

---
+++ sample/application/library/action/Action.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Sample\Application
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+
+	/**
+	 * @package Ramverk
+	 * @subpackage Action
+	 *
+	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+	 * @copyright (c) 2013-2014, Authors
+	 */
+	class Action
+	{
+	}
+}
+// End of file: Action.class.php
+// Location: sample/application/library/action/Action.class.php
---
+++ sample/application/library/view/View.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Sample\Application
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+
+	/**
+	 * @package Ramverk
+	 * @subpackage View
+	 *
+	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+	 * @copyright (c) 2013-2014, Authors
+	 */
+	class View
+	{
+	}
+}
+// End of file: View.class.php
+// Location: sample/application/library/view/View.class.php
