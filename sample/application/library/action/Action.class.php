5d27a462e1aa1481fbca4639fd9141bc74a6b162
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-01-25 09:14:29 +0100

    Minor inheritance adjustments to the sample application library.

--- sample/application/library/action/Action.class.php
+++ sample/application/library/action/Action.class.php
@@ -4,15 +4,13 @@ namespace Me\Raatiniemi\Ramverk\Sample\Application
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk;
 
 	/**
-	 * @package Ramverk
-	 * @subpackage Action
-	 *
 	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
 	 * @copyright (c) 2013-2014, Authors
 	 */
-	class Action
+	class Action extends Ramverk\Action
 	{
 	}
 }
--- sample/application/library/view/View.class.php
+++ sample/application/library/view/View.class.php
@@ -4,15 +4,13 @@ namespace Me\Raatiniemi\Ramverk\Sample\Application
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk;
 
 	/**
-	 * @package Ramverk
-	 * @subpackage View
-	 *
 	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
 	 * @copyright (c) 2013-2014, Authors
 	 */
-	class View
+	class View extends Ramverk\View
 	{
 	}
 }
