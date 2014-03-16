21c8b96278628d79981fc47908ad17eb8eefce6e
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-03-16 14:24:32 +0100

    Added the Routing class and Web related request, response, and routing.

---
+++ src/library/routing/Routing.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Me\Raatiniemi\Ramverk
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+
+	class Routing
+	{
+		public function __construct()
+		{
+		}
+	}
+}
+// End of file: Routing.class.php
+// Location: library/routing/Routing.class.php
