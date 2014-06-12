2412ca88f8c7ab65f8a93e6e9a119b76389f8b97
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-06-12 10:02:46 +0200

    Modified the module configuration handler.
    Instead of parsing through the entire document only the configuration group
    should be parsed.
    
    Also, prefixed every setting with "module." from the module configuration to
    avoid conflicts with other configuration items.

--- src/library/configuration/handler/Module.class.php
+++ src/library/configuration/handler/Module.class.php
@@ -22,15 +22,16 @@ namespace Me\Raatiniemi\Ramverk\Configuration\Handler
 		/**
 		 * Execute the configuration handler.
 		 * @param Me\Raatiniemi\Ramverk\Data\Dom\Document $document XML document with configuration data.
-		 * @throws Me\Raatiniemi\Ramverk\Exception If no module group have been defined.
+		 * @throws Me\Raatiniemi\Ramverk\Exception If no configuration group have been defined.
 		 * @return array Retrieved configuration data.
 		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
 		 */
 		public function execute(Dom\Document $document)
 		{
-			// Retrieve the module group from the document. Every document must
-			// have one module group, otherwise it's an invalid document.
-			$group = $document->getElementsByTagName('module');
+			// Retrieve the configuration group from the document. Every module
+			// configuration document must have one and only one configuration
+			// group, otherwise it's an invalid document.
+			$group = $document->getElementsByTagName('configuration');
 			if(empty($group) || $group->length <> 1) {
 				// TODO: Write exception message.
 				// TODO: Better specify the exception object.
@@ -38,7 +39,7 @@ namespace Me\Raatiniemi\Ramverk\Configuration\Handler
 			}
 
 			$data = array();
-			foreach($document->getElementsByTagName('setting') as $item) {
+			foreach($group->item(0)->getElementsByTagName('setting') as $item) {
 				// Every setting item must have the name attribute.
 				if(!$item->hasAttribute('name')) {
 					// TODO: Write exception message.
@@ -48,7 +49,7 @@ namespace Me\Raatiniemi\Ramverk\Configuration\Handler
 
 				// Retrieve the setting name and value.
 				$name = $item->getAttribute('name');
-				$data[$name] = $this->expandDirectives($item->getValue());
+				$data["module.{$name}"] = $this->expandDirectives($item->getValue());
 			}
 			return $data;
 		}
