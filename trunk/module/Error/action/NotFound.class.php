6b27cd6a3aa1408bc4086fa69a0c1cdada5b9abb
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-07-20 08:05:30 +0200

    Added the Error module with the NotFound action.

---
+++ trunk/module/Error/action/NotFound.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Trunk\Error\Action
+{
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+	use Me\Raatiniemi\Ramverk\Trunk;
+
+	class NotFound extends Trunk\Action
+	{
+		public function execute()
+		{
+			return 'Success';
+		}
+	}
+}
