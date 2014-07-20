b9a6b86fcf4fb0bf9cb34582f0aa132c136841ce
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-07-20 20:03:22 +0200

    Added additional methods for the DOM element.

--- src/library/data/dom/Element.class.php
+++ src/library/data/dom/Element.class.php
@@ -18,6 +18,40 @@ namespace Me\Raatiniemi\Ramverk\Data\Dom
 		// | Trait use-directives.                                            |
 		// +------------------------------------------------------------------+
 		use Utility\Value;
+
+		/**
+		 * Retrieve the name of the element.
+		 * @return string Name of the element.
+		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+		 */
+		public function getName()
+		{
+			return $this->nodeName;
+		}
+
+		/**
+		 * Retrieve child elements with specified name.
+		 * @param string $name Name of the children to retrieve.
+		 * @return DOMNodeList List with the DOM nodes.
+		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+		 */
+		public function get($name)
+		{
+			return $this->getChildren($name);
+		}
+
+		/**
+		 * Retrieve child elements with specified name.
+		 * @param string $name Name of the children to retrieve.
+		 * @return DOMNodeList List with the DOM nodes.
+		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+		 */
+		public function getChildren($name)
+		{
+			// Query for child elements with the specified name.
+			$query = sprintf('child::*[local-name() = "%s"]', $name);
+			return $this->ownerDocument->getXPath()->query($query, $this);
+		}
 	}
 }
 // End of file: Element.class.php
