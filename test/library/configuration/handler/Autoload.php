3f211dd0a9807bb0ce239eb4f5db3f71d20b9ed0
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-15 03:52:45 +0200

    Autoload.php: Added additional unit tests.

--- test/library/configuration/handler/Autoload.php
+++ test/library/configuration/handler/Autoload.php
@@ -49,6 +49,65 @@ class Autoload extends \PHPUnit_Framework_TestCase
             array('Foo' => 'Bar.php')
         );
     }
+
+    public function testGroupWithNamespace()
+    {
+        // Load the configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <autoloads namespace="Foo">
+                    <autoload name="Bar">Baz.php</autoload>
+                </autoloads>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Autoload($this->config->getMock());
+        $this->assertEquals(
+            $autoload->execute($document),
+            array('Foo\\Bar' => 'Baz.php')
+        );
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     * @expectedExceptionMessage
+     */
+    public function testAutoloadWithoutName()
+    {
+        // Load the configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <autoloads>
+                    <autoload foo="Bar">Baz.php</autoload>
+                </autoloads>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Autoload($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     * @expectedExceptionMessage
+     */
+    public function testAutoloadWithoutValue()
+    {
+        // Load the configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <autoloads>
+                    <autoload name="Foo" />
+                </autoloads>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Autoload($this->config->getMock());
+        $autoload->execute($document);
+    }
 }
 // End of file: Autoload.php
 // Location: test/library/configuration/handler/Autoload.php
