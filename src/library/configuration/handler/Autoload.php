02a20c1b4e02ed6bb49a7fb39a1f86d078f2aefc
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-15 03:19:17 +0200

    Converted the autoload configuration handler to follow PSR-1/PSR-2.

--- src/library/configuration/handler/Autoload.php
+++ src/library/configuration/handler/Autoload.php
@@ -1,58 +1,60 @@
 <?php
-namespace Me\Raatiniemi\Ramverk\Configuration\Handler {
+namespace Me\Raatiniemi\Ramverk\Configuration\Handler;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
-	use Me\Raatiniemi\Ramverk;
-	use Me\Raatiniemi\Ramverk\Configuration;
-	use Me\Raatiniemi\Ramverk\Data\Dom;
+use Me\Raatiniemi\Ramverk;
+use Me\Raatiniemi\Ramverk\Configuration;
+use Me\Raatiniemi\Ramverk\Data\Dom;
 
-	/**
-	 * Handler for autoload configuration.
-	 *
-	 * @package Ramverk
-	 * @subpackage Configuration
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class Autoload extends Configuration\Handler {
-		/**
-		 * Execute the configuration handler.
-		 * @param Me\Raatiniemi\Ramverk\Data\Dom\Document $document XML document with configuration data.
-		 * @throws Me\Raatiniemi\Ramverk\Exception If an autoload item is missing the name-attribute.
-		 * @return array Retrieved configuration data.
-		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-		 * @todo If the group do not have a defined namespace, the application namespace should be used?
-		 */
-		public function execute(Dom\Document $document) {
-			$data = array();
-			foreach($document->getConfigurationElements() as $configuration) {
-				foreach($configuration->get('autoloads') as $group) {
-					// If the autoload group has defined a namespace, then this
-					// namespace will prefix every class within the group.
-					$namespace = $group->hasAttribute('namespace') ? "{$group->getAttribute('namespace')}\\" : null;
+/**
+ * Handler for autoload configuration.
+ *
+ * @package Ramverk
+ * @subpackage Configuration
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class Autoload extends Configuration\Handler
+{
+    /**
+     * Execute the configuration handler.
+     * @param Me\Raatiniemi\Ramverk\Data\Dom\Document $document XML document with configuration data.
+     * @throws Me\Raatiniemi\Ramverk\Exception If an autoload item is missing the name-attribute.
+     * @return array Retrieved configuration data.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     * @todo If the group do not have a defined namespace, the application namespace should be used?
+     */
+    public function execute(Dom\Document $document)
+    {
+        $data = array();
+        foreach ($document->getConfigurationElements() as $configuration) {
+            foreach ($configuration->get('autoloads') as $group) {
+                // If the autoload group has defined a namespace, then this
+                // namespace will prefix every class within the group.
+                $namespace = $group->hasAttribute('namespace') ? "{$group->getAttribute('namespace')}\\" : null;
 
-					// Retrieve the autoload items from the group.
-					foreach($group->get('autoload') as $autoload) {
-						// Every item must have the name of the class defined.
-						if(!$autoload->hasAttribute('name')) {
-							// TODO: Write exception message.
-							// TODO: Better specify the exception object.
-							throw new Ramverk\Exception('');
-						}
+                // Retrieve the autoload items from the group.
+                foreach ($group->get('autoload') as $autoload) {
+                    // Every item must have the name of the class defined.
+                    if (!$autoload->hasAttribute('name')) {
+                        // TODO: Write exception message.
+                        // TODO: Better specify the exception object.
+                        throw new Ramverk\Exception('');
+                    }
 
-						// Prepend the group namespace (if any) to the class
-						// name, and expand the class path directives.
-						$name = "{$namespace}{$autoload->getAttribute('name')}";
-						$data[$name] = $this->expandDirectives($autoload->getValue());
-					}
-				}
-			}
-			// TODO: Should we throw an exception if $data is empty?
-			return $data;
-		}
-	}
+                    // Prepend the group namespace (if any) to the class
+                    // name, and expand the class path directives.
+                    $name = "{$namespace}{$autoload->getAttribute('name')}";
+                    $data[$name] = $this->expandDirectives($autoload->getValue());
+                }
+            }
+        }
+        // TODO: Should we throw an exception if $data is empty?
+        return $data;
+    }
 }
-// End of file: Autoload.class.php
-// Location: library/configuration/handler/Autoload.class.php
+// End of file: Autoload.php
+// Location: library/configuration/handler/Autoload.php
