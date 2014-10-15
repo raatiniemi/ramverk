558a75558583e10dc3fc1866feb8891aa4bb1a1f
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-15 19:36:02 +0200

    Added additional unit tests for the core configuration handler.

--- test/library/configuration/handler/Core.php
+++ test/library/configuration/handler/Core.php
@@ -61,6 +61,240 @@ class Core extends \PHPUnit_Framework_TestCase
             )
         );
     }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testWithoutSystemActions()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testWithoutDefaultSystemAction()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="404">
+                        <module>Error</module>
+                        <action>NotFound</action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testWithout404SystemAction()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module>Index</module>
+                        <action>Index</action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSystemActionWithoutName()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action>
+                        <module>Error</module>
+                        <action>NotFound</action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSystemActionWithoutModule()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <action>Index</action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSystemActionWithoutModuleValue()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module></module>
+                        <action>Index</action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSystemActionWithoutAction()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module>Index</module>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSystemActionWithoutActionValue()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module>Index</module>
+                        <action></action>
+                    </system_action>
+                </system_actions>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
+
+    public function testBasicSettings()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module>Index</module>
+                        <action>Index</action>
+                    </system_action>
+                    <system_action name="404">
+                        <module>Error</module>
+                        <action>NotFound</action>
+                    </system_action>
+                </system_actions>
+                <settings>
+                    <setting name="Foo">Bar</setting>
+                </settings>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $this->assertEquals(
+            $autoload->execute($document),
+            array(
+                'actions.default_module' => 'Index',
+                'actions.default_action' => 'Index',
+                'actions.404_module' => 'Error',
+                'actions.404_action' => 'NotFound',
+                'core.foo' => 'Bar'
+            )
+        );
+    }
+
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSettingWithoutName()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <system_actions>
+                    <system_action name="default">
+                        <module>Index</module>
+                        <action>Index</action>
+                    </system_action>
+                    <system_action name="404">
+                        <module>Error</module>
+                        <action>NotFound</action>
+                    </system_action>
+                </system_actions>
+                <settings>
+                    <setting>Foo</setting>
+                </settings>
+            </configuration>'
+        );
+
+        $autoload = new Handler\Core($this->config->getMock());
+        $autoload->execute($document);
+    }
 }
 // End of file: Core.php
 // Location: test/library/configuration/handler/Core.php
