80510f9f1da4365d1a5cf9961f746aa00f00a7c9
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-16 19:47:55 +0200

    Migrated unit test for module configuration handler to PHPUnit.

--- test/library/configuration/handler/Module.php
+++ test/library/configuration/handler/Module.php
@@ -1,91 +1,110 @@
 <?php
-namespace Me\Raatiniemi\Ramverk\Test\Configuration\Handler
-{
+namespace Me\Raatiniemi\Ramverk\Test\Configuration\Handler;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
-	use Me\Raatiniemi\Ramverk\Configuration\Handler;
-	use Me\Raatiniemi\Ramverk\Data\Dom;
+use Me\Raatiniemi\Ramverk\Configuration\Handler;
+use Me\Raatiniemi\Ramverk\Data\Dom;
 
-	\Mock::generate('\\Me\\Raatiniemi\\Ramverk\\Configuration', 'MockConfig');
+/**
+ * Unit test case for the module configuration handler.
+ *
+ * @package Ramverk
+ * @subpackage Test
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class Module extends \PHPUnit_Framework_TestCase
+{
+    // Stores the mock builder object for the Configuration-class.
+    private $config;
 
-	/**
-	 * Unit test case for the module configuration handler.
-	 *
-	 * @package Ramverk
-	 * @subpackage Test
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class Module extends \UnitTestCase
-	{
-		public function testSimple()
-		{
-			$document = new Dom\Document();
-			$document->loadXML(
-				'<configuration>
-					<settings>
-						<setting name="foo">bar</setting>
-					</settings>
-				</configuration>'
-			);
+    public function setUp()
+    {
+        $this->config = $this->getMockBuilder('Me\\Raatiniemi\\Ramverk\\Configuration');
+    }
 
-			$config = new \MockConfig();
-			$module = new Handler\Module($config);
+    public function tearDown()
+    {
+        $this->config = null;
+    }
 
-			$this->assertEqual($module->execute($document), array('module.foo' => 'bar'));
-		}
+    public function testBasicDocument()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <settings>
+                    <setting name="foo">Bar</setting>
+                    <setting name="baz">Quux</setting>
+                </settings>
+            </configuration>'
+        );
 
-		public function testEmptyDocument()
-		{
-			$document = new Dom\Document();
+        $module = new Handler\Module($this->config->getMock());
+        $this->assertEquals(
+            $module->execute($document),
+            array(
+                'module.foo' => 'Bar',
+                'module.baz' => 'Quux'
+            )
+        );
+    }
 
-			$config = new \MockConfig();
-			$module = new Handler\Module($config);
+    public function testEmptyDocument()
+    {
+        $document = new Dom\Document();
 
-			$this->assertEqual($module->execute($document), array());
-		}
+        $module = new Handler\Module($this->config->getMock());
+        $this->assertEquals($module->execute($document), array());
+    }
 
-		public function testSettingWithoutName()
-		{
-			$document = new Dom\Document();
-			$document->loadXML(
-				'<configuration>
-					<settings>
-						<setting foo="bar">baz</setting>
-					</settings>
-				</configuration>'
-			);
+    /**
+     * @expectedException Me\Raatiniemi\Ramverk\Exception
+     */
+    public function testSettingWithoutName()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <settings>
+                    <setting>Foo</setting>
+                </settings>
+            </configuration>'
+        );
 
-			$config = new \MockConfig();
-			$module = new Handler\Module($config);
+        $module = new Handler\Module($this->config->getMock());
+        $module->execute($document);
+    }
 
-			$this->expectException();
-			$module->execute($document);
-		}
+    public function testMultipleSections()
+    {
+        // Load the sample configuration to the document.
+        $document = new Dom\Document();
+        $document->loadXML(
+            '<configuration>
+                <settings>
+                    <setting name="foo">Bar</setting>
+                </settings>
+                <settings>
+                    <setting name="baz">Quux</setting>
+                </settings>
+            </configuration>'
+        );
 
-		public function testMultipleSettings()
-		{
-			$document = new Dom\Document();
-			$document->loadXML(
-				'<configuration>
-					<settings>
-						<setting name="foo">bar</setting>
-					</settings>
-					<settings>
-						<setting name="baz">qux</setting>
-					</settings>
-				</configuration>'
-			);
-
-			$config = new \MockConfig();
-			$module = new Handler\Module($config);
-
-			$data = array('module.foo' => 'bar', 'module.baz' => 'qux');
-			$this->assertEqual($module->execute($document), $data);
-		}
-	}
+        $module = new Handler\Module($this->config->getMock());
+        $this->assertEquals(
+            $module->execute($document),
+            array(
+                'module.foo' => 'Bar',
+                'module.baz' => 'Quux'
+            )
+        );
+    }
 }
 // End of file: Module.php
 // Location: test/library/configuration/handler/Module.php
