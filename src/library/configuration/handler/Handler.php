f44009cb8ad0635a738ecb4244d49f6c53ee6458
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-22 04:11:54 +0200

    Converted the configuration handler to PSR.

--- src/library/configuration/handler/Handler.php
+++ src/library/configuration/handler/Handler.php
@@ -1,58 +1,61 @@
 <?php
-namespace Me\Raatiniemi\Ramverk\Configuration {
+namespace Me\Raatiniemi\Ramverk\Configuration;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
-	use Me\Raatiniemi\Ramverk;
-	use Me\Raatiniemi\Ramverk\Data\Dom;
+use Me\Raatiniemi\Ramverk;
+use Me\Raatiniemi\Ramverk\Data\Dom;
 
-	/**
-	 * Base functionality for configuration handlers.
-	 *
-	 * @package Ramverk
-	 * @subpackage Configuration
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	abstract class Handler {
-		// +------------------------------------------------------------------+
-		// | Trait use-directives.                                            |
-		// +------------------------------------------------------------------+
-		use Utility;
+/**
+ * Base functionality for configuration handlers.
+ *
+ * @package Ramverk
+ * @subpackage Configuration
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+abstract class Handler
+{
+    // +------------------------------------------------------------------+
+    // | Trait use-directives.                                            |
+    // +------------------------------------------------------------------+
+    use Utility;
 
-		/**
-		 * Configuration container.
-		 * @var Me\Raatiniemi\Ramverk\Configuration\Container
-		 */
-		protected $config;
+    /**
+     * Configuration container.
+     * @var Me\Raatiniemi\Ramverk\Configuration\Container
+     */
+    protected $config;
 
-		/**
-		 * Initialize the configuration handler.
-		 * @param Me\Raatiniemi\Ramverk\Configuration\Container $config Configuration container.
-		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-		 */
-		public function __construct(Ramverk\Configuration $config) {
-			$this->config = $config;
-		}
+    /**
+     * Initialize the configuration handler.
+     * @param Me\Raatiniemi\Ramverk\Configuration\Container $config Configuration container.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
+    public function __construct(Ramverk\Configuration $config)
+    {
+        $this->config = $config;
+    }
 
-		/**
-		 * Get the configuration container, used by Utility-trait.
-		 * @return Me\Raatiniemi\Ramverk\Configuration\Container Configuration container.
-		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-		 */
-		protected function getConfig() {
-			return $this->config;
-		}
+    /**
+     * Get the configuration container, used by Utility-trait.
+     * @return Me\Raatiniemi\Ramverk\Configuration\Container Configuration container.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
+    protected function getConfig()
+    {
+        return $this->config;
+    }
 
-		/**
-		 * Execute the configuration handler.
-		 * @param Me\Raatiniemi\Ramverk\Data\Dom\Document $document XML document with configuration data.
-		 * @return array Retrieved configuration data.
-		 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-		 */
-		abstract public function execute(Dom\Document $document);
-	}
+    /**
+     * Execute the configuration handler.
+     * @param Me\Raatiniemi\Ramverk\Data\Dom\Document $document XML document with configuration data.
+     * @return array Retrieved configuration data.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
+    abstract public function execute(Dom\Document $document);
 }
 // End of file: Handler.php
 // Location: library/configuration/handler/Handler.php
