e925908e1e99cde552370ebd60566230f09f43b7
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-23 04:54:52 +0200

    Filesystem: Class and method documentation.

--- src/library/utility/Filesystem.php
+++ src/library/utility/Filesystem.php
@@ -7,6 +7,8 @@ namespace Me\Raatiniemi\Ramverk\Utility;
 use Me\Raatiniemi\Ramverk;
 
 /**
+ * Trait for working with the filesystem.
+ *
  * @package Ramverk
  * @subpackage Utility
  *
@@ -15,26 +17,60 @@ use Me\Raatiniemi\Ramverk;
  */
 trait Filesystem
 {
+    /**
+     * Check if the path is a directory.
+     * @param string $filename Path to check.
+     * @return boolean True if path is a directory, otherwise false.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
     public function isDirectory($filename)
     {
         return is_dir($filename);
     }
 
+    /**
+     * Check if the path is a file.
+     * @param string $filename Path to check.
+     * @return boolean True if path is a file, otherwise false.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
     public function isFile($filename)
     {
         return is_file($filename);
     }
 
+    /**
+     * Check if the path is readable.
+     * @param string $path Path to check.
+     * @return boolean True if path is readable, otherwise false.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
     public function isReadable($path)
     {
         return is_readable($path);
     }
 
+    /**
+     * Check if the path is writable.
+     * @param string $path Path to check.
+     * @return boolean True if path is writable, otherwise false.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
     public function isWritable($path)
     {
         return is_writable($path);
     }
 
+    /**
+     * Attempt to create directory at given path.
+     * @param string $pathname Directory path to create.
+     * @param integer $mode Permissions for the directory.
+     * @param boolean $recursive True for recursion, otherwise single level.
+     * @throws Me\Raatiniemi\Ramverk\Exception If path exists but is not a directory.
+     * @throws Me\Raatiniemi\Ramverk\Exception If unable to create directory.
+     * @return boolean True if directory was created or already exists, otherwise false.
+     * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+     */
     public function makeDirectory($pathname, $mode = 0777, $recursive = false)
     {
         $returnValue = true;
@@ -45,7 +81,8 @@ trait Filesystem
             // actually a directory and not a file.
             if (!$this->isDirectory($pathname)) {
                 // TODO: Write exception message.
-                throw new Ramverk\Exception();
+                // TODO: Better specify the exception object.
+                throw new Ramverk\Exception('');
             }
 
             // TODO: Check permissions, and attempt to fix if incorrect.
@@ -53,7 +90,8 @@ trait Filesystem
             // Attempt to create the directory.
             if (!($returnValue = mkdir($pathname, $mode, $recursive))) {
                 // TODO: Write exception message.
-                throw new Ramverk\Exception();
+                // TODO: Better specify the exception object.
+                throw new Ramverk\Exception('');
             }
         }
 
