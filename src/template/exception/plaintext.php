e18a4903c216a0fd464e6640107b8d6c23d54306
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-24 04:48:34 +0200

    Converted the plaintext exception template to PSR.

--- src/template/exception/plaintext.php
+++ src/template/exception/plaintext.php
@@ -1,6 +1,6 @@
 <?php
-namespace Me\Raatiniemi\Ramverk
-{
+namespace Me\Raatiniemi\Ramverk;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
@@ -8,43 +8,43 @@ namespace Me\Raatiniemi\Ramverk
 // +--------------------------------------------------------------------------+
 // | Global variable declaration.                                             |
 // +--------------------------------------------------------------------------+
-	global $e;
+global $e;
 
-	// If headers have not already been sent, we have to set the content type.
-	if(!headers_sent()) {
-		header('Content-Type: text/plain');
-	}
-
-	$className = get_class($e);
-	$length = strlen($className);
-?>
-===========<?=str_repeat('=', $length) . PHP_EOL; ?>
-Exception: <?=$className . PHP_EOL; ?>
-===========<?=str_repeat('=', $length) . PHP_EOL; ?>
-<?php if($e instanceof Exception) : ?>
-
-This is an internal Ramverk exception.
-<?php endif; ?>
-
-<?php
-	$message = sprintf(
-		'An exception of type "%s" was thrown, but did not get caught during the request execution.',
-		$className
-	);
-
-	echo wordwrap($message, 80, PHP_EOL) . PHP_EOL;
-?>
-
-Message
-=========
-<?=wordwrap(html_entity_decode($e->getMessage()), 80, PHP_EOL) . PHP_EOL; ?>
-
-Version Information
-=====================
-PHP: <?=phpversion() . PHP_EOL; ?>
-System: <?=php_uname() . PHP_EOL; ?>
-Timestamp: <?=gmdate(DATE_ISO8601) . PHP_EOL; ?>
-<?php
+// If headers have not already been sent, we have to set the content type.
+if (!headers_sent()) {
+    header('Content-Type: text/plain');
 }
+
+$className = get_class($e);
+$length = strlen($className);
+
+printf('===========%s%s', str_repeat('=', $length), PHP_EOL);
+printf('Exception: %s%s', $className, PHP_EOL);
+printf('===========%s%s', str_repeat('=', $length), PHP_EOL);
+//var_dump($e->getTrace());
+
+if($e instanceof Exception) :
+    printf('%1$sThis is an internal Ramverk exception.%1$s%1$s', PHP_EOL);
+endif;
+
+$message = sprintf(
+    'An exception of type "%s" was thrown, but did not get caught during the '.
+    'request execution.',
+    $className
+);
+echo wordwrap($message, 80, PHP_EOL), PHP_EOL;
+
+if(!empty($e->getMessage())) :
+    printf('%1$sMessage%1$s', PHP_EOL);
+    echo '=========', PHP_EOL;
+    echo wordwrap(html_entity_decode($e->getMessage()), 80, PHP_EOL), PHP_EOL;
+endif;
+
+printf('%1$sVersion Information%1$s', PHP_EOL);
+echo '=====================', PHP_EOL;
+printf('PHP: %s%s', phpversion(), PHP_EOL);
+printf('System: %s%s', php_uname(), PHP_EOL);
+printf('Timestamp: %s%s', gmdate(DATE_ISO8601), PHP_EOL);
+
 // End of file: plaintext.php
-// Location: template/exception/plaintext.php ?>
+// Location: template/exception/plaintext.php
