27a7d16f955be7f7692295f40e8cedc332350863
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-26 06:55:58 +0100

    Converted unit test for configuration utility to PHPUnit/PSR.

--- test/library/configuration/Utility.php
+++ test/library/configuration/Utility.php
@@ -1,53 +1,52 @@
 <?php
-namespace Me\Raatiniemi\Ramverk\Test\Configuration
-{
+namespace Me\Raatiniemi\Ramverk\Test\Configuration;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
-	use Me\Raatiniemi\Ramverk\Configuration as Config;
+use Me\Raatiniemi\Ramverk\Configuration as Config;
 
-	/**
-	 * Unit test case for the configuration utility.
-	 *
-	 * @package Ramverk
-	 * @subpackage Test
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class Utility extends \UnitTestCase
-	{
-		public function testExpandDirective()
-		{
-			$config = new Config();
-			$config->set('foo', 'bar');
+/**
+ * Unit test case for the configuration utility.
+ *
+ * @package Ramverk
+ * @subpackage Test
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class Utility extends \PHPUnit_Framework_TestCase
+{
+    public function testExpandDirective()
+    {
+        $config = new Config();
+        $config->set('foo', 'bar');
 
-			$this->assertEqual($config->expandDirectives('%foo%'), 'bar');
-		}
+        $this->assertEquals(Config\Utility::expand($config, '%foo%'), 'bar');
+    }
 
-		public function testExpandDirectives()
-		{
-			$config = new Config();
-			$config->set('foo', 'bar');
-			$config->set('baz', 'qux');
+    public function testExpandDirectives()
+    {
+        $config = new Config();
+        $config->set('foo', 'bar');
+        $config->set('baz', 'qux');
 
-			$this->assertEqual($config->expandDirectives('%foo%%baz%'), 'barqux');
-		}
+        $this->assertEquals(Config\Utility::expand($config, '%foo%%baz%'), 'barqux');
+    }
 
-		public function testExpandDirectiveWithoutDirectives()
-		{
-			$config = new Config();
+    public function testExpandDirectiveWithoutDirectives()
+    {
+        $config = new Config();
 
-			$this->assertEqual($config->expandDirectives('%foo%'), '%foo%');
-		}
+        $this->assertEquals(Config\Utility::expand($config, '%foo%'), '%foo%');
+    }
 
-		public function testExpandDirectiveWithoutReference()
-		{
-			$config = new Config();
+    public function testExpandDirectiveWithoutReference()
+    {
+        $config = new Config();
 
-			$this->assertEqual($config->expandDirectives('foo'), 'foo');
-		}
-	}
+        $this->assertEquals(Config\Utility::expand($config, 'foo'), 'foo');
+    }
 }
 // End of file: Utility.php
 // Location: test/library/configuration/Utility.php
