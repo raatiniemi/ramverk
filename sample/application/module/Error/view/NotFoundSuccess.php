f76b97396c2eb06057b478b25565bfc23c63ebc5
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-30 05:04:03 +0100

    Added the Error module.

---
+++ sample/application/module/Error/view/NotFoundSuccess.php
@@ -0,0 +1,17 @@
+<?php
+namespace Me\Raatiniemi\Ramverk\Sample\Application\Error\View;
+
+// +--------------------------------------------------------------------------+
+// | Namespace use-directives.                                                |
+// +--------------------------------------------------------------------------+
+use Me\Raatiniemi\Ramverk\Sample\Application;
+
+/**
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class NotFoundSuccess extends Application\View
+{
+}
+// End of file: NotFoundSuccess.php
+// Location: sample/application/module/Error/view/NotFoundSuccess.php
