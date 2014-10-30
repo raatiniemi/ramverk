542a5398a25867cc46d923b6c9997aa597b238b1
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-10-30 04:11:34 +0100

    Converted the sample application base view to PSR.

--- sample/application/library/view/View.class.php
+++ sample/application/library/view/View.class.php
@@ -1,18 +1,19 @@
 <?php
-namespace Me\Raatiniemi\Ramverk\Sample\Application
-{
+namespace Me\Raatiniemi\Ramverk\Sample\Application;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
-	use Me\Raatiniemi\Ramverk;
+use Me\Raatiniemi\Ramverk;
 
-	/**
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class View extends Ramverk\View
-	{
-	}
+/**
+ * Base view for sample application.
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class View extends Ramverk\View
+{
 }
+
 // End of file: View.class.php
 // Location: sample/application/library/view/View.class.php
