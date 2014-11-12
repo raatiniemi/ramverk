4ee35387918f679fddc5499479c364f6eb15d807
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-11-12 19:27:59 +0100

    Converted the action class to PSR.

--- src/library/action/Action.php
+++ src/library/action/Action.php
@@ -1,22 +1,21 @@
 <?php
-namespace Me\Raatiniemi\Ramverk
-{
+namespace Me\Raatiniemi\Ramverk;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
 
-	/**
-	 * Base for handling controller actions.
-	 *
-	 * @package Ramverk
-	 * @subpackage Action
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class Action
-	{
-	}
+/**
+ * Base for handling controller actions.
+ *
+ * @package Ramverk
+ * @subpackage Action
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class Action
+{
 }
 // End of file: Action.php
 // Location: library/action/Action.php
