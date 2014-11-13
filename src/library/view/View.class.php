b28f4a668c4f8b33a79d41e32914ba2874b5d74a
Tobias Raatiniemi <raatiniemi@gmail.com>
2014-11-13 03:07:27 +0100

    Converted the view class to PSR.

--- src/library/view/View.class.php
+++ src/library/view/View.class.php
@@ -1,22 +1,21 @@
 <?php
-namespace Me\Raatiniemi\Ramverk
-{
+namespace Me\Raatiniemi\Ramverk;
+
 // +--------------------------------------------------------------------------+
 // | Namespace use-directives.                                                |
 // +--------------------------------------------------------------------------+
 
-	/**
-	 * Base for handling controller views.
-	 *
-	 * @package Ramverk
-	 * @subpackage View
-	 *
-	 * @author Tobias Raatiniemi <raatiniemi@gmail.com>
-	 * @copyright (c) 2013-2014, Authors
-	 */
-	class View
-	{
-	}
+/**
+ * Base for handling controller views.
+ *
+ * @package Ramverk
+ * @subpackage View
+ *
+ * @author Tobias Raatiniemi <raatiniemi@gmail.com>
+ * @copyright (c) 2013-2014, Authors
+ */
+class View
+{
 }
 // End of file: View.class.php
 // Location: library/view/View.class.php
